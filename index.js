d2c7686039435435665eff28fc482458de560352
Chidinma Onwuchuruba <105228116+Nmaleo@users.noreply.github.com>
2023-09-10 03:03:18 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+let server = express();
+let weekdays = [
+  "Monday",
+  "Tuesday,",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+server.get("/api", (req, res) => {
+  let responce = {
+    slack_name: req.query.slack_name,
+    current_day: weekdays[new Date().getDay() - 1],
+    utc_time: new Date().toISOString().split(".")[0] + "Z",
+    track: req.query.track,
+    github_file_url:
+      "https://github.com/Nmaleo/HNG_task/index.js",
+    github_repo_url: "https://github.com/Nmaleo/HNG_task",
+    status_code: 200,
+  };
+  res.json(responce);
+});
+
+server.listen(9000, () => {
+  console.log("server running on port 9000");
+});
