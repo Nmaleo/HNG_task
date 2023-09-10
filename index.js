0f5e5f0d04b6ae2bb7b68ac3a2b59edc7ea65f4c
Chidinma Onwuchuruba <105228116+Nmaleo@users.noreply.github.com>
2023-09-10 06:31:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ let getutc = () => {
 server.get("/api", (req, res) => {
   let responce = {
     slack_name: req.query.slack_name,
-    current_day: "Sunday"
+    current_day: "Sunday",
     utc_time: new Date().toISOString().split(".")[0] + "Z",
     track: req.query.track,
     github_file_url:
